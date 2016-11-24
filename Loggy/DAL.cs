dd747fe3a2de6ccc824b07dbb72eb04bc9eacb18
ststeiger <quandary82@hailmail.net>
2016-11-24 17:38:42 +0100

    Added  various methods

--- Loggy/DAL.cs
+++ Loggy/DAL.cs
@@ -204,6 +204,269 @@ namespace Loggy
         } // End Template InlineTypeAssignHelper
 
 
+
+        public static string GetParameters(System.Data.IDbCommand cmd)
+        {
+            string strReturnValue = "";
+            try
+            {
+                System.Text.StringBuilder msg = new System.Text.StringBuilder();
+                System.DateTime dtLogTime = System.DateTime.UtcNow;
+
+                if (cmd == null || string.IsNullOrEmpty(cmd.CommandText))
+                {
+                    return strReturnValue;
+                }
+
+
+                if (cmd.Parameters != null && cmd.Parameters.Count > 0)
+                {
+                    msg.AppendLine("-- ***** Listing Parameters *****");
+
+                    foreach (System.Data.IDataParameter idpThisParameter in cmd.Parameters)
+                    {
+                        // http://msdn.microsoft.com/en-us/library/cc716729.aspx
+                        msg.AppendLine(string.Format("DECLARE {0} AS {1} -- DbType: {2}", idpThisParameter.ParameterName, SqlTypeFromDbType(idpThisParameter.DbType), idpThisParameter.DbType.ToString()));
+                    } // Next idpThisParameter
+
+                    msg.AppendLine(Environment.NewLine);
+                    msg.AppendLine(Environment.NewLine);
+
+                    foreach (System.Data.IDataParameter idpThisParameter in cmd.Parameters)
+                    {
+                        string strParameterValue = null;
+                        if (object.ReferenceEquals(idpThisParameter.Value, System.DBNull.Value))
+                        {
+                            strParameterValue = "NULL";
+                        }
+                        else
+                        {
+                            if (idpThisParameter.DbType == System.Data.DbType.Date)
+                                strParameterValue = String.Format(DATEFORMAT, idpThisParameter.Value);
+                            else if (idpThisParameter.DbType == System.Data.DbType.DateTime || idpThisParameter.DbType == System.Data.DbType.DateTime2)
+                                strParameterValue = String.Format(DATETIMEFORMAT, idpThisParameter.Value);
+                            else
+                                strParameterValue = idpThisParameter.Value.ToString();
+
+                            strParameterValue = "'" + strParameterValue.Replace("'", "''") + "'";
+                        }
+
+                        msg.AppendLine(string.Format("SET {0} = {1}", idpThisParameter.ParameterName, strParameterValue));
+                    }
+
+                    msg.AppendLine("-- ***** End Parameter Listing *****");
+                    msg.AppendLine(Environment.NewLine);
+                } // End if (cmd.Parameters != null && cmd.Parameters.Count > 0)
+
+                strReturnValue = msg.ToString();
+                msg = null;
+            }
+            catch (Exception ex)
+            {
+                strReturnValue = "Error in Function COR.SQL.GetParametrizedQueryText";
+                strReturnValue += Environment.NewLine;
+                strReturnValue += ex.Message;
+            }
+
+            return strReturnValue;
+        } // End Function GetParametrizedQueryText
+
+
+        public static string GetParametrizedQueryText(System.Data.IDbCommand cmd)
+        {
+            string strReturnValue = "";
+            try
+            {
+                System.Text.StringBuilder msg = new System.Text.StringBuilder();
+                System.DateTime dtLogTime = System.DateTime.UtcNow;
+
+                if (cmd == null || string.IsNullOrEmpty(cmd.CommandText))
+                {
+                    return strReturnValue;
+                }
+
+
+                if (cmd.Parameters != null && cmd.Parameters.Count > 0)
+                {
+                    msg.AppendLine("-- ***** Listing Parameters *****");
+
+                    foreach (System.Data.IDataParameter idpThisParameter in cmd.Parameters)
+                    {
+                        // http://msdn.microsoft.com/en-us/library/cc716729.aspx
+                        msg.AppendLine(string.Format("DECLARE {0} AS {1} -- DbType: {2}", idpThisParameter.ParameterName, SqlTypeFromDbType(idpThisParameter.DbType), idpThisParameter.DbType.ToString()));
+                    } // Next idpThisParameter
+
+                    msg.AppendLine(Environment.NewLine);
+                    msg.AppendLine(Environment.NewLine);
+
+                    foreach (System.Data.IDataParameter idpThisParameter in cmd.Parameters)
+                    {
+                        string strParameterValue = null;
+                        if (object.ReferenceEquals(idpThisParameter.Value, System.DBNull.Value))
+                        {
+                            strParameterValue = "NULL";
+                        }
+                        else
+                        {
+                            if (idpThisParameter.DbType == System.Data.DbType.Date)
+                                strParameterValue = String.Format(DATEFORMAT, idpThisParameter.Value);
+                            else if (idpThisParameter.DbType == System.Data.DbType.DateTime || idpThisParameter.DbType == System.Data.DbType.DateTime2)
+                                strParameterValue = String.Format(DATETIMEFORMAT, idpThisParameter.Value);
+                            else
+                                strParameterValue = idpThisParameter.Value.ToString();
+
+                            strParameterValue = "'" + strParameterValue.Replace("'", "''") + "'";
+                        }
+
+                        msg.AppendLine(string.Format("SET {0} = {1}", idpThisParameter.ParameterName, strParameterValue));
+                    }
+
+                    msg.AppendLine("-- ***** End Parameter Listing *****");
+                    msg.AppendLine(Environment.NewLine);
+                } // End if (cmd.Parameters != null && cmd.Parameters.Count > 0)
+
+
+                msg.AppendLine(string.Format("-- ***** Start Query from {0} *****", dtLogTime.ToString(DATEFORMAT)));
+                msg.AppendLine(cmd.CommandText);
+                msg.AppendLine(string.Format("-- ***** End Query from {0} *****", dtLogTime.ToString(DATEFORMAT)));
+                msg.AppendLine(Environment.NewLine);
+
+                strReturnValue = msg.ToString();
+                msg = null;
+            }
+            catch (Exception ex)
+            {
+                strReturnValue = "Error in Function COR.SQL.GetParametrizedQueryText";
+                strReturnValue += Environment.NewLine;
+                strReturnValue += ex.Message;
+            }
+
+            return strReturnValue;
+        } // End Function GetParametrizedQueryText
+
+
+        public static System.Data.Common.DbConnection GetConnection()
+        {
+            return GetConnection(null);
+        } // End Function GetConnection
+
+
+        public static System.Data.Common.DbConnection GetConnection(string strConnectionString)
+        {
+            if (string.IsNullOrEmpty(strConnectionString))
+            {
+                strConnectionString = GetConnectionString();
+            }
+
+            System.Data.Common.DbConnection idbcon = m_ProviderFactory.CreateConnection();
+            idbcon.ConnectionString = strConnectionString;
+
+            return idbcon;
+        } // End Function GetConnection
+
+        public static System.Data.IDbCommand CreateCommand(string strSQL)
+        {
+            return CreateCommand(strSQL, 30);
+        }
+
+
+
+        public static System.Data.IDbCommand CreateCommand(string strSQL, int timeout)
+        {
+            System.Data.IDbCommand idbc = m_ProviderFactory.CreateCommand();
+
+            if (IsPostGreSql)
+            {
+                strSQL = ConvertTopNStatement(strSQL);
+                strSQL = ReplaceOdbcFunctions(strSQL);
+            }
+
+            if (!string.IsNullOrEmpty(strSQL))
+            {
+                idbc.CommandText = strSQL;
+            }
+
+            idbc.CommandTimeout = Math.Max(timeout, 0);
+
+            return idbc;
+        } // End Function CreateCommand
+
+
+        public static System.Data.IDbCommand CreateCommand()
+        {
+            return CreateCommand(null);
+        } // End Function CreateCommand
+
+
+        // Warning: getMandant() liefert immer den entsprechenden Mandant...
+        //public static System.Data.IDbCommand CreateCommandFromFile(string strEmbeddedFileName)
+        //{
+        //    Int32 intBE_ID = -1;
+        //    // Mandant manMandant = Mandant.Global;
+        //    Mandant manMandant = getMandant();
+        //    return CreateCommandFromFile(strEmbeddedFileName, intBE_ID, manMandant);
+        // }
+
+        public static System.Data.IDbCommand CreateCommandFromFile(string strEmbeddedFileName, Int32 intBE_ID)
+        {
+            return CreateCommandFromFile(strEmbeddedFileName, intBE_ID, 30);
+        }
+
+
+        public static System.Data.IDbCommand CreateCommandFromFile(string strEmbeddedFileName, Int32 intBE_ID, int timeOut)
+        {
+            //Mandant enuMandant = Mandant.Global;
+            Mandant enuMandant = SQL.getMandant();
+
+            return CreateCommandFromFile(strEmbeddedFileName, intBE_ID, enuMandant, timeOut);
+        }
+
+
+
+        public static System.Data.IDbCommand CreateCommandFromFile(string strEmbeddedFileName, Mandant enuMandant)
+        {
+            return CreateCommandFromFile(strEmbeddedFileName, enuMandant, 30);
+        }
+
+
+        public static System.Data.IDbCommand CreateCommandFromFile(string strEmbeddedFileName, Mandant enuMandant, int timeOut)
+        {
+            Int32 intBE_ID = -1;
+
+            return CreateCommandFromFile(strEmbeddedFileName, intBE_ID, enuMandant, timeOut);
+        }
+
+
+        public static System.Data.IDbCommand CreateCommandFromFile(string strEmbeddedFileName, Int32 intBE_ID, Mandant enuMandant)
+        {
+            return CreateCommandFromFile(strEmbeddedFileName, intBE_ID, enuMandant, 30);
+        }
+
+
+        public static System.Data.IDbCommand CreateCommandFromFile(string strEmbeddedFileName, Int32 intBE_ID, Mandant enuMandant, int Timeout)
+        {
+            //Start: Rico Test
+            if (!string.IsNullOrEmpty(strEmbeddedFileName) && !strEmbeddedFileName.StartsWith(".")) strEmbeddedFileName = "." + strEmbeddedFileName;
+            //End: Rico Test
+
+            string strRessourceName = string.Empty;
+            string strSQL = GetEmbeddedSqlScript(strEmbeddedFileName, enuMandant, ref strRessourceName);
+
+            System.Data.IDbCommand tCommand = CreateCommand(strSQL);
+            tCommand.CommandTimeout = Timeout;
+
+            strRessourceName = strRessourceName.Substring(strRessourceName.IndexOf('.') + 1);
+            strRessourceName = strRessourceName.Substring(strRessourceName.IndexOf('.'));
+
+            AddParameter(tCommand, "@___ResourceName", strRessourceName);
+            AddParameter(tCommand, "@BE_ID", intBE_ID);
+            AddParameter(tCommand, "@MDT_ID", (int)enuMandant);
+
+            return tCommand;
+        }
+
+
+
         public void ErrorLog(string sPathName, string sErrMsg)
         {
             string sErrorTime = System.DateTime.Now.ToString("yyyyMMdd'T'HHmmss", System.Globalization.CultureInfo.InvariantCulture);
