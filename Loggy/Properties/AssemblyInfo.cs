1d1de010c67602b1aeca0d089b677976037dd20d
ststeiger <quandary82@hailmail.net>
2016-12-02 19:38:13 +0100

    Custom Error pages

--- Loggy/Properties/AssemblyInfo.cs
+++ Loggy/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // Sie können alle Werte angeben oder die standardmäßigen Revisions- und Buildnummern 
 // übernehmen, indem Sie "*" wie folgt verwenden:
-[assembly: AssemblyVersion("2016.12.1.1708")]
-[assembly: AssemblyFileVersion("2016.12.1.1708")]
+[assembly: AssemblyVersion("2016.12.2.1926")]
+[assembly: AssemblyFileVersion("2016.12.2.1926")]
