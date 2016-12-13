dd7003a580aaa19e1727c2757b9cb903acc9adf8
ststeiger <quandary82@hailmail.net>
2016-12-13 14:25:23 +0100

    Update Text to English

--- Loggy/closure/multi_parent_example/04b_Insert_Data_PGSQL.sql
+++ Loggy/closure/multi_parent_example/04b_Insert_Data_PGSQL.sql
@@ -15,16 +15,16 @@ SELECT setval('T_Comments_com_id_seq', (SELECT COALESCE(MAX(com_id), 1) FROM T_C
 
 
 
-INSERT INTO T_Comments(COM_Id, COM_Text) VALUES (1, N'Alle');
-INSERT INTO T_Comments(COM_Id, COM_Text) VALUES (2, N'Kunde A');
+INSERT INTO T_Comments(COM_Id, COM_Text) VALUES (1, N'All');
+INSERT INTO T_Comments(COM_Id, COM_Text) VALUES (2, N'Customer A');
 INSERT INTO T_Comments(COM_Id, COM_Text) VALUES (3, N'Admin');
 INSERT INTO T_Comments(COM_Id, COM_Text) VALUES (4, N'Normal');
-INSERT INTO T_Comments(COM_Id, COM_Text) VALUES (5, N'Stao 1&2');
-INSERT INTO T_Comments(COM_Id, COM_Text) VALUES (6, N'Stao 1');
-INSERT INTO T_Comments(COM_Id, COM_Text) VALUES (7, N'Stao 2');
-INSERT INTO T_Comments(COM_Id, COM_Text) VALUES (8, N'Stao 3');
-INSERT INTO T_Comments(COM_Id, COM_Text) VALUES (9, N'Stao 4');
-INSERT INTO T_Comments(COM_Id, COM_Text) VALUES (10, N'Stao 1&3');
+INSERT INTO T_Comments(COM_Id, COM_Text) VALUES (5, N'Site 1&2');
+INSERT INTO T_Comments(COM_Id, COM_Text) VALUES (6, N'Site 1');
+INSERT INTO T_Comments(COM_Id, COM_Text) VALUES (7, N'Site 2');
+INSERT INTO T_Comments(COM_Id, COM_Text) VALUES (8, N'Site 3');
+INSERT INTO T_Comments(COM_Id, COM_Text) VALUES (9, N'Site 4');
+INSERT INTO T_Comments(COM_Id, COM_Text) VALUES (10, N'Site 1&3');
 
 
 
