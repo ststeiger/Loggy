3f5d1591ceb0ffe9fddec38fdd9087144e94f205
root <quandary82@hailmail.net>
2016-12-13 08:45:09 +0100

    returning

--- Loggy/closure/multi_parent_example/01_Schema_PG.sql
+++ Loggy/closure/multi_parent_example/01_Schema_PG.sql
@@ -32,3 +32,55 @@ CREATE TABLE IF NOT EXISTS T_Comments_Closure
 
 GO
 
+-- Function: insertcomment(bigint, bigint)
+
+-- DROP FUNCTION insertcomment(bigint, bigint);
+
+CREATE OR REPLACE FUNCTION insertcomment(__parent bigint, __child bigint)
+  RETURNS void AS
+$BODY$ 
+DECLARE
+	__maxStart timestamp without time zone;  
+	__minEnd timestamp without time zone;  
+	__interval int; 
+BEGIN 
+	INSERT INTO T_Comments_Closure
+	(
+		 ancestor
+		,descendant
+		,depth
+	)
+	SELECT 
+		 p.ancestor AS Parent 
+		,c.descendant AS Child 
+		,p.depth + c.depth + 1 AS Calcdepth 
+	FROM T_Comments_Closure AS p 
+
+	INNER JOIN T_Comments_Closure AS c 
+		-- ON p.descendant = @__parent AND c.ancestor = @__child 
+		ON p.descendant = __parent AND c.ancestor = __child 
+	;
+	-- RETURN 0;
+END; 
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION insertcomment(bigint, bigint)
+  OWNER TO postgres;
+
+
+
+
+DROP TABLE IF EXISTS bar;
+
+CREATE TEMPORARY TABLE bar AS
+WITH CTE AS
+(
+	INSERT INTO T_Comments (com_text)
+	SELECT 'abc'
+	UNION SELECT 'def' 
+	RETURNING com_id, 'abc'::text as foo 
+) 
+SELECT * FROM CTE 
+
+SELECT * FROM bar 
