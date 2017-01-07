f25dd10936c3ac5212c0a5eb6aa18cb0fbb87e7d
Me <me@localhost.com>
2017-01-07 17:08:12 +0100

    info

--- Loggy/Code/Mail/info.cs
+++ Loggy/Code/Mail/info.cs
@@ -58,6 +58,141 @@ namespace Loggy
         public System.Collections.Specialized.NameValueCollection Session;
 
 
+
+        public class CookieValues
+        {
+            public string Name;
+            public string Value;
+            public string Path;
+            public System.DateTime Expires;
+
+            public bool Secure;
+            public bool HttpOnly;
+
+
+            public CookieValues(System.Web.HttpCookie c)
+            {
+
+                this.Name = c.Name;
+                this.Value = c.Value;
+                this.Path = c.Path;
+                this.Expires = c.Expires;
+                this.Secure = c.Secure;
+                this.HttpOnly = c.HttpOnly;
+
+            }
+
+
+        }
+
+
+
+        public static void GatherCookieData(System.Web.HttpContext context)
+        {
+            System.Collections.Generic.Dictionary<string, CookieValues> dict = new System.Collections.Generic.Dictionary<string, CookieValues>(System.StringComparer.OrdinalIgnoreCase);
+
+            foreach (System.Web.HttpCookie thisCookie in context.Request.Cookies)
+            {
+                dict.Add(c.Name, new CookieValues(thisCookie));
+            } // Next thisCookie 
+
+        } // End Function GatherCookieData 
+
+
+        public static System.Collections.Generic.Dictionary<string, object> GatherUserSessionInfo()
+        {
+            System.Collections.Generic.Dictionary<string, object> Session =
+                new System.Collections.Generic.Dictionary<string, object>();
+
+
+            System.Data.SqlClient.SqlConnectionStringBuilder csb = new System.Data.SqlClient.SqlConnectionStringBuilder();
+            csb.DataSource = System.Environment.MachineName;
+            csb.InitialCatalog = "COR_Basic";
+            
+            csb.IntegratedSecurity = true;
+            if (!csb.IntegratedSecurity)
+            {
+                csb.UserID = "";
+                csb.Password = "";
+            }
+            csb.ApplicationName = "Loggy";
+            // csb.CurrentLanguage = "";
+            csb.PacketSize = 4096;
+            csb.PersistSecurityInfo = false;
+            csb.Pooling = true;
+            csb.MaxPoolSize = 5;
+            csb.WorkstationID = System.Environment.MachineName;
+
+            System.Data.DataTable dt = new System.Data.DataTable();
+            string sql = "SELECT TOP 2 * FROM T_Benutzer";
+            System.Data.SqlClient.SqlDataAdapter da = new System.Data.SqlClient.SqlDataAdapter(sql, csb.ConnectionString);
+            da.Fill(dt);
+
+            System.Web.HttpContext.Current.Session["foo"] = dt;
+            System.Web.HttpContext.Current.Session["bar"] = dt.Rows[0];
+
+
+
+            if (System.Web.HttpContext.Current != null && System.Web.HttpContext.Current.Session != null)
+            {
+                foreach (string strSessionKey in System.Web.HttpContext.Current.Session.Keys)
+                {
+                    Session[strSessionKey] = System.Web.HttpContext.Current.Session[strSessionKey];
+                } // Next strSessionKey 
+
+            } // End if (System.Web.HttpContext.Current != null && System.Web.HttpContext.Current.Session != null)
+
+
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject(Session, Newtonsoft.Json.Formatting.Indented);
+            System.Console.WriteLine(json);
+
+            return Session;
+        } // End Function GatherUserSessionInfo 
+
+        public static void foo(System.Web.HttpContext context)
+        {
+
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject(context.Request.Headers, Newtonsoft.Json.Formatting.Indented);
+            System.Console.WriteLine(json);
+            // context.Request.RequestType
+            // context.Request.ServerVariables
+            // context.Request.Url
+            // context.Request.UrlReferrer;
+            // context.Request.UserAgent;
+            // context.Request.UserHostAddress;
+            // context.Request.UserHostAddress;
+            // context.Request.UserLanguages;
+
+            // context.Application.AllKeys
+            // context.ApplicationInstance.all
+            // context.ApplicationInstance.Site
+            // context.Cache
+            foreach (object x in context.Cache)
+            {
+                System.Console.WriteLine(x.GetType());
+            }
+            context.Request.PathInfo
+            //context.Request.InputStream
+            context.Request.ContentType
+            context.Request.ContentEncoding
+            context.Request.Browser
+            context.Request.ApplicationPath
+
+            foreach (string header in context.Request.Headers.AllKeys)
+            {
+                string value = context.Request.Headers[header];
+            }
+
+
+            foreach (string serverVariable in context.Request.ServerVariables.AllKeys)
+            {
+                string value = context.Request.ServerVariables[serverVariable];
+            }
+        }
+
+
+
+
         public string ClientIP;
         public string ForwardedClientIP;
 
